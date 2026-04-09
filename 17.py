bd3bf20e03c816bcfb4975a84348042645892287
awkward-merc <awkward2017@yahoo.co.jp>
2026-04-09 19:53:41 +0900

    追加：17

---
+++ 17.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 21:06:27 2026
+
+@author: awk77
+"""
+
+a = 10
+b = 3.14
+c = "Hello, Python!"
+d = True
+e = ["a", "b", "c"]
+f = ("a", "b", "c")
+g = {"a", "b", "c"}
+h = {"a": 1, "b": 2, "c": 3}
+
+print(f'a：{a},データ型：{type(a)}')
+print(f'b：{b},データ型：{type(b)}')
+print(f'b：{c},データ型：{type(c)}')
+print(f'd：{d},データ型：{type(d)}')
+print(f'e：{e},データ型：{type(e)}')
+print(f'f：{f},データ型：{type(f)}')
+print(f'g：{g},データ型：{type(g)}')
+print(f'h：{h},データ型：{type(h)}')
