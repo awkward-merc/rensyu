ee6c742fb08d4a51b54b05abbf40e9905765579e
awkward-merc <awkward2017@yahoo.co.jp>
2026-04-09 17:30:27 +0900

    14を追加

---
+++ 14.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 20:54:41 2026
+
+@author: awk77
+"""
+
+x="Python"
+print(f'x：{x},文字数：{len(x)}')
+
+print('------\n')
+
+y=12345
+y=str(y)
+print(len(y)) #数値型だとエラー
