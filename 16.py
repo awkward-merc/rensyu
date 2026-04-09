58febb671a1e0b45905be99ca7973984c274b4f1
awkward-merc <awkward2017@yahoo.co.jp>
2026-04-09 17:43:20 +0900

    16を追加

---
+++ 16.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 21:02:43 2026
+
+@author: awk77
+"""
+
+x=None
+print(f'x：{x},データ型：{type(x)}')
+
+print('------')
+
+y='None'
+print(f'y：{y},データ型：{type(y)}')
