bc35923112dcef83c440c61cdee16000397f2cef
awkward-merc <awkward2017@yahoo.co.jp>
2026-04-09 17:38:48 +0900

    15を追加

---
+++ 15.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 20:57:55 2026
+
+@author: awk77
+"""
+
+x="Python"
+print(x)
+print(x[0:2])
+print(f' {x[1:3]}')
+print(f'   {x[3:-1]}')
+print(f'     {x[5]}')
+print(x[0:100]) #文字数より長い場合は最後まで
