3c611e29cb8d02bfee9972c981fd9b21e6ebd888
awkward_miniPC <awkward2017@yahoo.co.jp>
2026-04-11 15:23:25 +0900

    追加：2

---
+++ 2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 21:07:37 2026
+
+@author: awk_w
+"""
+
+print("1")
+print("2")
+print("3")
+
+print(1)
+print(2)
+print(3)
+
+print('1')
+print('2')
+print('3')
