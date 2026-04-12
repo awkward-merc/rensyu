1ea04d0a91020adf13addf8a99c4801a545172ac
awkward_miniPC <awkward2017@yahoo.co.jp>
2026-04-12 10:33:21 +0900

    追加:4

---
+++ 4.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 21:11:40 2026
+
+@author: awk_w
+"""
+
+#加工前
+print(1)
+print(2)
+print(3)
+
+print('------\n')
+
+#加工後
+print(1)
+# print(2)
+print(3)
